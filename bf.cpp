5b0824812056d6806147006e0844edbb9a034601
Not really <32463657+WampiFlampi@users.noreply.github.com>
2021-01-05 23:52:47 -0500

    Create bf.cpp
    if you absolutely need to compile this use clang

---
+++ bf.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+
+
+int main() {
+  
+  //program instructions
+  string init = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++------------>++++++[<+++++++++>-]<+<+++-------------->>>++++[<++++++++>-]<+" ;
+  int len = init.length();
+
+  // memory allocated to bf
+  vector<int> mem(10,0);
+  
+ //convert input to char array
+  char source[len];
+  strcpy(source ,init.c_str());
+  
+
+  //map bracket location
+  vector<int> bmap;
+
+  for(int n=0 ;n<len;n++) {
+    switch(source[n]) {
+      
+      case '[':
+        bmap.push_back(n);
+        break;
+      
+      case ']':
+        bmap.push_back(n);
+        break;
+      
+      default:
+        break;
+
+  }
+  }
+
+  //pointer for mem
+  int point = 0;
+
+  //stores bmap location
+  int nav = 0;
+  
+  //iterates through input
+
+  for(int n=0 ;n<len;n++) {
+  
+    switch(source[n]) {
+      
+      //defining languauge rules
+      case '>':
+        point++;
+        break;
+      case '<':
+        point--;
+        break;
+      case '+':
+        mem[point]++;
+        break;
+      case '-':
+        mem[point]--;
+        break;
+
+
+      //altering the n value which corresponds to location in the program instructions
+      //loop behaviour
+
+      case '[':
+        if ( mem[point] == 0) {
+          n = bmap[nav+1];
+          nav = nav + 2;
+        }
+        break;
+      
+      case ']':
+        if ( mem[point]>0) {
+          n = bmap[nav]-1;
+        }else {
+          nav = nav+2;
+        }
+       
+      
+  
+      
+
+    }   
+  
+  }
+
+  
+for (int i = 0; i < mem.size();i++) {
+    cout << "<";
+    cout << mem[i];
+    cout << ">";
+    cout << "\n";
+  }
+  
+}
