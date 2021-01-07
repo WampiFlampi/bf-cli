009e736ebb1df68d7333100f7277057d74be4083
Not really <32463657+WampiFlampi@users.noreply.github.com>
2021-01-06 23:32:21 -0500

    Update and rename bf-arch.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include "engine.h"
+
+
+using namespace std;
+
+
+int main() {
+  
+  //program instructions
+  string init = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+." ;
+  int len = init.length();
+
+  
+  
+ //convert input to char array
+  char source[len];
+  strcpy(source ,init.c_str());
+  
+
+  //map bracket location
+  vector<int> bmap;
+
+  for(int n=0 ;n<len;n++) {
+    switch(source[n]) {
+      
+      case '[':
+        bmap.push_back(n);
+        break;
+      
+      case ']':
+        bmap.push_back(n);
+        break;
+      
+      default:
+        break;
+
+  }
+  }
+
+  
+
+  //iterates through input
+
+  iterate(bmap, len, source);
+
+  
+  cout << endl;
+  return 0;
+  
+}
