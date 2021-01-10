653a3d1743fad3362505e93bff18f69a54e18d89
Not really <32463657+WampiFlampi@users.noreply.github.com>
2021-01-10 02:52:22 -0500

    comments

--- graphics.h
+++ graphics.h
@@ -7,27 +7,30 @@
 using namespace std;
 
 void init() {
-		initscr();
-		noecho();
+	initscr();
+	noecho();
 }
 
 void end() {
 	endwin();
 }
 
+//frame renderer
+
 void display(vector<int> mem, int pnt) {
 
+  //erases previous frame from memory	
   erase();
+	
+  //see ncurses documentation	
   move(10,pnt*4);
   printw("^");	
 
   for (int i = 0;i < mem.size();i++) {
-		 
-		  move(9,4*i);
-		  printw("%d",mem[i]);
-		  
+	move(9,4*i);
+	printw("%d",mem[i]);
   
-		}
+  }
   refresh();
   
 }
