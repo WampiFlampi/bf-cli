76ac15b89710887102e7cff1c969cc3a461cd74f
Not really <32463657+WampiFlampi@users.noreply.github.com>
2021-01-18 23:37:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,28 @@
 #include <bits/stdc++.h>
+#include <fstream>
 #include "src/engine.h"
 
 
 using namespace std;
 
 
-int main() {
+int main(int argc,char *argv[]) {
   
   //program instructions
-  string init = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+." ;
-  int len = init.length();
+  ifstream input;
+  
+  input.open(argv[1],ifstream::in);
+  
+  //c++.com wildin
+  filebuf* buffer = input.rdbuf();
+  size_t size = buffer->pubseekoff (0,input.end,input.in);
+  buffer->pubseekpos (0,input.in);
+
+  char source[size];
+  input.read(source,size);
+  
 
   
-  
- //convert input to char array
-  char source[len];
-  strcpy(source ,init.c_str());
-  
-
-  //map bracket location
-  vector<int> bmap;
-
-  for(int n=0 ;n<len;n++) {
-    switch(source[n]) {
-      
-      case '[':
-        bmap.push_back(n);
-        break;
-      
-      case ']':
-        bmap.push_back(n);
-        break;
-      
-      default:
-        break;
-
-  }
-  }
-
   
 
   //iterates through input
